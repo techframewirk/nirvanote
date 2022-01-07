e4418372d9ab78f4c0b98eb1ba9644391fdd50d1
Sandeep Kumar <sandeepmahendran@gmail.com>
2022-01-08 00:26:48 +0530

    Removed unused code

--- utils/whatsapp.js
+++ utils/whatsapp.js
@@ -15,20 +15,6 @@ const authenticateWithWhatsapp = async () => {
         } else {
             let authHeader = `${process.env.WHATSAPP_ADMIN_USERNAME}:${process.env.WHATSAPP_ADMIN_PASSWORD}`
             let authHeaderBase64 = Buffer.from(authHeader).toString('base64')
-            // let config = {
-            //     method: 'POST',
-            //     url: `${process.env.WHATSAPP_URL}/v1/users/login`,
-            //     headers: {
-            //         'Authorization': `Basic ${authHeaderBase64}`,
-            //         'Content-Type': 'application/json'
-            //     },
-            // }
-            // axios(config).then((response) => {
-            //     cache.cacheKeyValue(cache.cacheConstants.whatsappToken, response.data.users[0].token)
-            //     token = response.data.users[0].token
-            // }).catch((err) => {
-            //     throw err
-            // })
             let response = await axios.post(`${process.env.WHATSAPP_URL}/v1/users/login`, {}, {
                 headers: {
                     'Authorization': `Basic ${authHeaderBase64}`,
@@ -154,5 +140,6 @@ const sendMessage = async (message) => {
 module.exports = {
     authenticateWithWhatsapp,
     setWhatsappWebhook,
+    sendMessage,
     listenToWhatsapp
 }
